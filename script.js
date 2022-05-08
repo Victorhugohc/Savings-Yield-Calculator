f54c616f435ba016a5dd1c9824c6afcd24e11b09
Víctor Hugo Hernández Cabrera <87137545+Victorhugohc@users.noreply.github.com>
2022-05-08 16:22:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
-t = 10; // Estimated years of investment [min=1]years
-u = 50; // Daily deposits [min=0]$
-v = 12; // Estimated yield per year [min=0]%
-w = 5; // Annual increase in daily deposits [min=0]%
-x = 0; // Initial investment amount [min=0]$
-y = 1 + (w/100); // Calculation annual increase in daily deposits
-z = 1 + ((v/100)/365); // Calculation of yield per day based on yield per year
+function Calculation(){
 
-for (j=0; j < t; j++) {
+    var t = 1; // Initialize variable
+    var u = 0; // Initialize variable
+    var v = 0; // Initialize variable
+    var w = 0; // Initialize variable
+    var x = 0; // Initialize variable
 
-    for (i=0; i < 365; i++) {
+    var t = document.getElementById('years').value; // Estimated years of investment [min=1]years
+    var u = document.getElementById('daily').value; // Daily deposits [min=0]$
+    var v = document.getElementById('yield').value; // Estimated yield per year [min=0]%
+    var w = document.getElementById('increase').value; // Annual increase in daily deposits [min=0]%
+    var x = document.getElementById('start').value; // Initial investment amount [min=0]$
 
-        x = (x * z) + u;
+    var y = 1 + (w/100); // Calculation annual increase in daily deposits
+    var z = 1 + ((v/100)/365); // Calculation of yield per day based on yield per year
+
+    for (j=0; j < t; j++) {
+
+        u *= y;
+
+        for (i=0; i < 365; i++) {
+
+            x = (x * z) + u;
+
+        }
 
     }
-
-    u *= y;
+    
+    document.getElementById('result').value = x.toFixed(2);
 
 }
-
-console.log(x); // Result
