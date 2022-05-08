63c15e528812a8c33ab799e5bf1371bf001fc8ef
Víctor Hugo Hernández Cabrera <87137545+Victorhugohc@users.noreply.github.com>
2022-05-08 14:46:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ u = 50; // Daily deposits [min=0]$
 v = 12; // Estimated yield per year [min=0]%
 w = 5; // Annual increase in daily deposits [min=0]%
 x = 0; // Initial investment amount [min=0]$
-y = 1 + (w/100); // Calculation annual increase (For Loop)
+y = 1 + (w/100); // Calculation annual increase in daily deposits
 z = 1 + ((v/100)/365); // Calculation of yield per day based on yield per year
 
 for (j=0; j < t; j++) {
